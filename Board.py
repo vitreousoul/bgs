bd4125e0b6ac7498aad1babdeb903f3b43273756
zgkos <zachgianikos@gmail.com>
2023-06-11 14:41:45 -0400

    Added __init__.py and creaed board_test.py to test the Board class

--- Board.py
+++ Board.py
@@ -343,21 +343,3 @@ class Board:
         # TODO: def validate_move(self):
         # or generate the entire list of legal moves to compare against
         
-        
-            
-""" Test of current features"""
-# board = Board('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
-board = Board()
-print(board)
-while True:
-    
-    if board.white_to_move:
-        user_move = input("Enter a move for White: ")
-    else:
-        user_move = input("Enter a move for Black: ")
-    
-    if user_move.lower() == 'q' or user_move.lower() == 'quit':
-        break
-    
-    board.move(user_move)
-    
