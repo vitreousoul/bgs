1472e27d871b837e0ae25e92c2a69e76e8ff9509
Ryan Merritt <merrittaudio@gmail.com>
2023-06-12 21:39:55 -0500

    parse pgn tokens into a simple result object

--- pgn.py
+++ pgn.py
@@ -3,15 +3,15 @@ PGN Parser
 http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm
 """
 
-from enum import Enum
+from enum import IntEnum
 from dataclasses import dataclass
 
 # TODO: we haven't parsed yet, but once we do, delete ParserState if it is not used
-class ParserState(Enum):
+class ParserState(IntEnum):
     TAG_PAIR = 0   # DOCS 8.1: Tag pair section
     MOVE_TEXT = 1  # DOCS 8.2: Movetext section
 
-class TokenKind(Enum):
+class TokenKind(IntEnum):
     NONE = 0
     # use ASCII value for single-char kinds to make single-char tokenizing easier
     PERIOD = ord('.')
@@ -141,8 +141,73 @@ def parse_symbol_token(pgn_source, index):
     return token, index
 
 def parse_pgn(pgn_source):
+    token_index = 0
+    move_number = 1
+    parser_state = ParserState.TAG_PAIR
+    pgn_result = { 'tag_pairs': {}, 'moves': [] }
+    pgn_result_code = 0
     tokens = tokenize_pgn(pgn_source)
-    return tokens
+    while tokens[token_index] != TokenKind.END_OF_STREAM:
+        if parser_state == ParserState.TAG_PAIR:
+            if tokens[token_index].kind == TokenKind.INTEGER:
+                # we assume that TAG_PAIRs are defined before MOVE_TEXT,
+                # so if we see at the start of parsing a potential TAG_PAIR,
+                # then we move on to parsing MOVE_TEXT.
+                parser_state = ParserState.MOVE_TEXT
+            elif token_index + 3 >= len(tokens):
+                print(f"Not enough tokens to parse TAG_PAIR, last token is {tokens[-1]}")
+                pgn_result_code = 1
+            else:
+                bracket_open_token = tokens[token_index]
+                symbol_token = tokens[token_index+1]
+                string_token = tokens[token_index+2]
+                bracket_close_token = tokens[token_index+3]
+                if (bracket_open_token.kind == TokenKind.BRACKET_OPEN and
+                    symbol_token.kind == TokenKind.SYMBOL and
+                    string_token.kind == TokenKind.STRING and
+                    bracket_close_token.kind == TokenKind.BRACKET_CLOSE):
+                    # save the TAG_PAIR with the symbol token as the key and the string token as the value
+                    pgn_result['tag_pairs'][symbol_token.text] = string_token.text
+                    token_index += 4
+                else:
+                    print(f"Malformed TAG_PAIR with tokens [{bracket_open_token.kind} {symbol_token.kind} {string_token.kind} {bracket_close_token.kind}]")
+                    pgn_result_code = 1
+                    break
+        elif parser_state == ParserState.MOVE_TEXT:
+            if tokens[token_index].kind == TokenKind.INTEGER:
+                # move numbers can repeat, but must always be defined sequentially
+                if tokens[token_index].number == move_number + 1:
+                    # new move number is one more than the current move number, that is ok
+                    move_number += 1
+                    token_index += 1
+                elif tokens[token_index].number != move_number:
+                    print(f"Invalid move number at current move number {move_number} and given error move number {tokens[token_index].number}")
+                    pgn_result_code = 1
+                    break
+                else:
+                    # We only get here if the token number we jsut parsed is equal to the current move number,
+                    # so there isn't much to do except pass over the token
+                    token_index += 1
+                # eat period tokens after a move number
+                while True:
+                    if tokens[token_index].kind == TokenKind.PERIOD:
+                        token_index += 1
+                    else:
+                        break
+            elif tokens[token_index].kind == TokenKind.NUMERIC_ANNOTATION_GLYPH:
+                # ignore NAGs for now
+                token_index += 1
+            elif tokens[token_index].kind == TokenKind.SYMBOL:
+                # any other symbol is a move, EXCEPT the final symbol which should be the game-result tag
+                pgn_result['moves'].append(tokens[token_index].text)
+                token_index += 1
+            else:
+                break
+        else:
+            print(f"parse_pgn unknown state {parser_state}")
+            pgn_result_code = 1
+            break
+    return pgn_result, pgn_result_code
 
 def is_space(char):
     return (char == ' ' or char == '\n' or char == '\r' or char == '\t')
@@ -178,8 +243,7 @@ def test(arg_object):
     pgn_file = open('./assets/1992_11_04_fischer_spassky.pgn', 'r')
     pgn_source = pgn_file.read()
     pgn_file.close()
-    tokens = parse_pgn(pgn_source)
+    pgn_result, result_code = parse_pgn(pgn_source)
     if 'verbose' in arg_object:
-        for token in tokens:
-            print(token)
-    return 0 if len(tokens) > 0 else 1
+        print(pgn_result)
+    return result_code
