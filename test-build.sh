f56b1939564057ee10a812f5b459d17ad349a48a
Ryan Merritt <merrittaudio@gmail.com>
2023-06-19 11:36:09 -0500

    give exe extension to executable file so we can gitignore it

--- test-build.sh
+++ test-build.sh
@@ -4,4 +4,4 @@ SOURCE_FILE="test.c"
 SETTINGS="-std=c99 -Wall -Wextra -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations"
 LIBS=`/usr/local/Cellar/python@3.11/3.11.2_1/bin/python3.11-config --cflags --ldflags --embed`
 
-gcc -o testing $SOURCE_FILE $SETTINGS $LIBS
+gcc -o testing.exe $SOURCE_FILE $SETTINGS $LIBS
