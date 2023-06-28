5633b03c43de5fb2ab3cb57484c790606c3f0fac
EJ <44304689+ejgipson@users.noreply.github.com>
2023-06-27 21:04:43 -0700

    Removed \t\t to prevent infinite loop
    This is why god invented ENDIF

--- RESUME.py
+++ RESUME.py
@@ -19,23 +19,23 @@ while True:
     except ValueError:
             continue
     else:
-                if resume == 'N' or resume == 'n':
-                    # NEW should overwrite a previous save.log file 
-                    # NEW should execute board_test.py as written
-                    print("Starting NEW game, previous saved data destroyed.")
-                    board = Board()
-                    print(board)
-                    break
-                elif resume == 'R' or resume == 'r':
-                    # RESUME Sr. board = Board([Last line FEN])
-                    # RESUME Jr. board = Board('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
-                    # RESUME should open the existing save.log file and append information
-                    print("RESUMing previous game.")
-                    break
-                elif resume == 'Q' or resume == 'q':
-                    print("Eat glass, soy boy.")
-                    break
-                elif resume != 'N' and resume != 'n' and resume != 'R' and resume != 'r':
-                    print("Sorry, I didn't understand that.")
-                    continue
+        if resume == 'N' or resume == 'n':
+            # NEW should overwrite a previous save.log file 
+            # NEW should execute board_test.py as written
+            print("Starting NEW game, previous saved data destroyed.")
+            board = Board()
+            print(board)
+            break
+        elif resume == 'R' or resume == 'r':
+            # RESUME Sr. board = Board([Last line FEN])
+            # RESUME Jr. board = Board('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1')
+            # RESUME should open the existing save.log file and append information
+            print("RESUMing previous game.")
+            break
+        elif resume == 'Q' or resume == 'q':
+            print("Eat glass, soy boy.")
+            break
+        else:
+            print("Sorry, I didn't understand that.")
+            continue
     
